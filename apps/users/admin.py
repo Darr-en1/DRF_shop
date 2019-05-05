99401aeacace7655bcaed27614591755aa48d11f
darr_en1 <darr_en1@126.com>
2019-05-05 14:33:57 +0800

    first commit

---
+++ apps/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from apps.users.models import UserProfile, VerifyCode
+
+admin.site.register(UserProfile)
+admin.site.register(VerifyCode)
